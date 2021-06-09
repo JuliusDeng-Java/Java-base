9834e3e717c74cdf15e357623f85b703c147c4f8
JuliusDeng <1576554007@qq.com>
2021-06-09 13:32:36 +0800

    三种注释。

---
+++ day-01/HelloJava.java
@@ -0,0 +1,17 @@
+/*
+	这是多行注释
+*/
+
+/**
+	这是文档注释
+	@author 邓宇辰
+	@version v1.0.0
+*/
+public	class  HelloJava
+{
+	public static void main(String[] args) 
+	{
+		// 单行注释：这是一个输出语句。
+		System.out.println("Hello World!");
+	}
+}
