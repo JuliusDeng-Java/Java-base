0317c0937b43e7df0b566e6881947af6e7e48203
JuliusDeng <1576554007@qq.com>
2021-06-11 13:12:31 +0800

    变量。

---
+++ editplus-day5/day-03/VariavleTest.java
@@ -0,0 +1,12 @@
+class VariavleTest 
+{
+	public static void main(String[] args) 
+	{
+		byte b1 = 11;
+		byte b2 = -124;
+		System.out.println(b1);
+		System.out.println(b2);
+		long l1 = 3244343434343434l;
+		System.out.println(l1);
+	}
+}
